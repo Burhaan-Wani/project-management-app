2206bd19efda6414ac04d7880624d36f2ca59ba7
Burhaan-U-Deen wani <burhaanwani823@gmail.com>
2025-01-23 19:22:59 +0530

    add member model

---
+++ backend/src/models/member.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document } from "mongoose";
+
+export interface MemberDocument extends Document {
+  userId: mongoose.Schema.Types.ObjectId;
+  workspaceId: mongoose.Schema.Types.ObjectId;
+  // role:RoleDocument
+  joinedAt: Date;
+}
+
+const memberSchema = new mongoose.Schema<MemberDocument>({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  workspaceId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  //   role: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "Role",
+  //     required: true,
+  //   },
+  joinedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const MemberModel = mongoose.model<MemberDocument>("Member", memberSchema);
+
+export default MemberModel;
